7f255991535de63fa9af4b25284ddf39b4d3fe43
MELLPUEN <168023551+MELLPUEN@users.noreply.github.com>
2024-04-24 14:54:22 -0400

    Add files via upload

---
+++ medicamento-1.pl
@@ -0,0 +1,10 @@
+:- dynamic disponible/2.
+disponible(tableta,metronidazol).
+disponible(tableta,ciproflozacino).
+disponible(tableta,acido_folico).
+disponible(tableta,omeprazol).
+disponible(tableta,ranitidina).
+disponible(tableta,loratadina).
+disponible(pomada,mebendazol).
+disponible(pomada,gentamicina).
+disponible(unguento,piperazina).
